d1561b79f1b624633202b33db30ad62f55e3830d
ana <ana.licina@plusplusnt.rs>
2021-01-06 16:59:30 +0100

    Dodate ideje za meni

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,7 +4,10 @@ import classes from './Navigation.module.css';
 
 const navigation = () => (
     <ul className={classes.Navigation}>
-        <NavigationItem link="/" active>Home</NavigationItem>
+        <NavigationItem link="/" active>Opste</NavigationItem>
+        <NavigationItem link="/">Biografija</NavigationItem>
+        <NavigationItem link="/">Igrice</NavigationItem>
+        <NavigationItem link="/">Moja ideja</NavigationItem>
         <NavigationItem link="/">#ZaShonetovBubreg</NavigationItem>
     </ul>
 );
